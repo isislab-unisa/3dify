cb39fb2f056ca9d854fc30d093b9bea849dd2351
tizianocitro <tizianocitro98@gmail.com>
2024-03-11 11:19:45 +0000

    fix: fixed code format

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            <span className='mr-4'>By{' '}</span>
+            <span className='mr-4'>By </span>
             <Image
               src='/isislab.png'
               alt='ISISLab Logo'
@@ -26,20 +26,20 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 text-8xl">
+      <div className="relative z-[-1] flex place-items-center text-8xl before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40">
         3Dify
       </div>
 
       {/* TODO: Add link to GitHub */}
-      <div className='mb-32 flex justify-content text-center lg:text-left'>
+      <div className='justify-content mb-32 flex text-center lg:text-left'>
         <a
-          href=''
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-white-100'
+          href='https://github.com/isislab-unisa/3dify-cloud-native'
+          className='hover:bg-white-100 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300'
           target='_blank'
           rel='noopener noreferrer'
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            GitHub{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
               -&gt;
             </span>
