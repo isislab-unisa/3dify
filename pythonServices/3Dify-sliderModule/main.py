6324b436efbad2f702ba5d0e010937a664510e72
Ater_mor <48551962+AndreaAmorosini@users.noreply.github.com>
2024-05-20 13:56:40 +0200

    Aggiunta servizio per il recupero dei landmarks, aggiunto slider fisso per forehead scaling

--- pythonServices/3Dify-sliderModule/main.py
+++ pythonServices/3Dify-sliderModule/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from imageProcess import process
-from faceShapeFeatures import inferFaceShapeSliders
+from imageProcess import process, extractLandmarks
 
 
 class Request(BaseModel):
     imageBase64: str | None = None
+    gender : int | None = None
+    age : int | None = None
 
 class Response(BaseModel):
     sliders: dict | None = None
+    landmarks: list | None = None
+    normalizedLandmarks: list | None = None
 
 app = FastAPI()
 
@@ -17,10 +20,16 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/extractLandmarks")
+async def getLandmarks(request: Request) -> Response:
+    imageBase64 = request.imageBase64
+    imageBase64 = imageBase64.split(",")[1]
+    landmarks, normalizedLandmarks = extractLandmarks(imageBase64)
+    return Response(landmarks=landmarks, normalizedLandmarks=normalizedLandmarks)
 
 @app.post("/generateSliders")
 async def generateSliders(request: Request) -> Response:
     imageBase64 = request.imageBase64
     imageBase64 = imageBase64.split(",")[1]
-    sliders = process(imageBase64)
+    sliders = process(imageBase64, request.gender, request.age)
     return Response(sliders=sliders)
