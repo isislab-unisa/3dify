6093aed0ffdb517a464dedcf8bc92a95688c0dc7
Tiziano <tizianocitro98@gmail.com>
2024-09-30 09:50:05 +0200

    feat: assign picture to user is now enabled

---
+++ app/api/addImageId/route.ts
@@ -0,0 +1,32 @@
+import { connectToMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
+import { revalidatePath } from "next/cache";
+
+type Body = {
+    email: string;
+    imageId: string;
+};
+
+export async function POST(req: Request) {
+    try {
+        await connectToMongoDB();
+        const user: Body = await req.json();
+
+        const existingUser = await User.findOne({ email: user.email });
+        if (!existingUser) {
+            return Response.json({ message: 'user does not exist.' });
+        }
+
+        if (existingUser.imageIds.includes(user.imageId)) {
+            return Response.json({ message: 'image id already exists.' });
+        }
+
+        await User.updateOne({ email: user.email }, { $push: { imageIds: [user.imageId] } });
+
+        revalidatePath("/");
+        return Response.json({ message: 'added image id to the user successfully.' });
+    } catch (error) {
+        console.log(error);
+        return Response.json({ message: 'error adding image id to the user.' });
+    }
+}
